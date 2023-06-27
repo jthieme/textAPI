9fae5aab8b24c79a520a5da439817029a68082c5
Joshua Thieme <jomthieme@gmail.com>
2023-06-26 20:21:01 -0600

    did it work?

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const Redis = require("redis");
 const app = express();
 const port = 3000;
 
+// const redisClient = Redis.createClient({url:"redis://127.0.0.1:6379"});
 const redisClient = Redis.createClient({url:"redis://default:62b621ee499146d785273842cad2590f@usw2-on-cat-30260.upstash.io:30260"});
+const redisConnect = redisClient.connect();
 
 app.use(bodyParser.json());
 
 app.listen(port, async () => {
-    const redisConnect = await redisClient.connect();
 
     redisConnect;
 
